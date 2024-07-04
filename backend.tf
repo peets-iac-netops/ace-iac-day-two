165144ebc695eac56ecb6a7470bf10b73ad906ff
Peet van de Sande <peet@peetvandesande.com>
2024-07-04 22:56:45 +0200

    Changed organization to use the name

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-    organization = "ws-3dfuSpK9o9Ge4VMW"
+    organization = "ace-iac-day-two"
     workspaces {
       name = "ace-iac-day-two"
     }
