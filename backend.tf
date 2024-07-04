ab75ab522dd2ab44a263d1cf6d7e922c5e261ed2
peets-iac-netops <162779868+peets-iac-netops@users.noreply.github.com>
2024-07-04 23:09:23 +0200

    Update backend.tf
    Changed organization name

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
-    organization = "ace-iac-day-two"
+    organization = "Peets-IAC"
     workspaces {
       name = "ace-iac-day-two"
     }
